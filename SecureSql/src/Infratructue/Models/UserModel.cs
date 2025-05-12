91c20aaf9a8466f1cb7b5c1460648500b8ca0582
emil476m <emilwiltmann.dk@gmail.com>
2025-05-12 12:28:05 +0200

    added a maxlength to name in usermodels

--- SecureSql/src/Infratructue/Models/UserModel.cs
+++ SecureSql/src/Infratructue/Models/UserModel.cs
@@ -1,7 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Infratructue.Models;
 
 public class UserModel
 {
     public Guid Id { get; set; }
+    
+    [MaxLength(255)]
     public string Name { get; set; }
 }
