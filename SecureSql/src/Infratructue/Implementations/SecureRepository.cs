6eed2421e5d09f0ac50234d3d225792e166f1842
emil476m <emilwiltmann.dk@gmail.com>
2025-05-12 12:07:33 +0200

    created interface and getuser for repository

--- SecureSql/src/Infratructue/Implementations/SecureRepository.cs
+++ SecureSql/src/Infratructue/Implementations/SecureRepository.cs
@@ -1,4 +1,42 @@
-﻿public class UnsecureRepository
+﻿using Api.Controllers;
+using Dapper;
+using Infratructue.Mappers;
+using Infratructue.Models;
+using Npgsql;
+
+public class UnsecureRepository : IUnsecureRepository
 {
+    private readonly NpgsqlDataSource _dataSource;
     
+    public UnsecureRepository(NpgsqlDataSource dataSource)
+    {
+        _dataSource = dataSource;
+    }
+
+    public async Task<UserModel> GetUserByName(string name)
+    {
+        var sql = $@"SELECT 
+            id as {nameof(UserDbModel.Id)},
+            name as {nameof(UserDbModel.Name)}
+            FROM users WHERE name = @name";
+        
+        using var conn = _dataSource.OpenConnection();
+        var result = conn.QueryFirst<UserDbModel>(sql, new { name });
+        return result.ToModel();
+    }
+
+    public Task<UserModel> CreateUser(UserModel user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> UpdateUser(UserModel user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteUserById(Guid id)
+    {
+        throw new NotImplementedException();
+    }
 }
--- SecureSql/src/Infratructue/Interfaces/ISecureRepository.cs
+++ SecureSql/src/Infratructue/Interfaces/ISecureRepository.cs
@@ -1,6 +1,16 @@
-﻿namespace Api.Controllers;
+﻿using Infratructue.Models;
+
+namespace Api.Controllers;
 
 public interface IUnsecureRepository
 {
+    Task<UserModel> GetUserByName(string name);
+    
+    Task<UserModel> CreateUser(UserModel user);
+    
+    Task<bool> UpdateUser(UserModel user);
+    
+    Task<bool> DeleteUserById(Guid id);
+    
     
 }
