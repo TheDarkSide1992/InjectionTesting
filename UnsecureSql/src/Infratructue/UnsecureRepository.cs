48917f4635a3c01149d6b8ec96a85476553e63ca
emil476m <emilwiltmann.dk@gmail.com>
2025-05-12 12:51:27 +0200

    Implemented very basic unsecurerepository

---
+++ UnsecureSql/src/Infratructue/UnsecureRepository.cs
@@ -0,0 +1,52 @@
+using Dapper;
+using Infratructue.Models;
+using Npgsql;
+
+namespace Infratructue;
+
+public class UnsecureRepository
+{
+    private readonly NpgsqlDataSource _dataSource;
+    
+    public UnsecureRepository(NpgsqlDataSource dataSource)
+    {
+        _dataSource = dataSource;
+    }
+    
+    public async Task<string> GetUserByName(string name)
+    {
+        var sql = "SELECT * FROM users WHERE name = " + name;
+        
+        using var conn = _dataSource.OpenConnection();
+        return conn.QueryFirst(sql);
+    }
+
+    public async Task<string> CreateUser(UserModel user)
+    {
+        var sql = "INSERT INTO users (id, name) VALUES (" + Guid.NewGuid() +", " + user.Name + ") RETURNING *";
+        
+        using var conn = _dataSource.OpenConnection();
+        
+        return await conn.QueryFirstAsync(sql);
+    }
+
+    public async Task<string> UpdateUser(UserModel user)
+    {
+        var sql = "UPDATE users SET name = " + user.Name + " WHERE id = " + user.Id + " RETURNING *";
+        
+        using var conn = _dataSource.OpenConnection();
+        
+        return await conn.QueryFirstAsync(sql);
+    }
+
+    public async Task<bool> DeleteUserById(Guid id)
+    {
+        var sql = $@"DELETE FROM users WHERE id = " + id;
+        
+        using var conn = _dataSource.OpenConnection();
+        
+        var result = await conn.ExecuteAsync(sql) == 1;
+
+        return result;
+    }
+}
