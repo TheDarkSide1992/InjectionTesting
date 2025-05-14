727ad8f887d18bf824237664151b7937e6b9ec80
Jens <55052002+TheDarkSide1992@users.noreply.github.com>
2025-05-14 14:28:36 +0200

    Api/feat/endpoints (#4)
    * Unsecure enpoints
    
    * added secure controller
    
    * updated method names

--- SecureSql/src/Api/Program.cs
+++ SecureSql/src/Api/Program.cs
@@ -1,9 +1,15 @@
+using Api.Controllers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddNpgsqlDataSource(Environment.GetEnvironmentVariable("pgconn")!,
+    dataSourceBuilder => dataSourceBuilder.EnableParameterLogging());
+
+builder.Services.AddScoped<ISecureRepository, SecureRepository>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
